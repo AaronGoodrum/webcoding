af44c6d7eeeef1c9a7a4e6064e106dc46f56cc1e
Aaron <30958265+GoodZillo@users.noreply.github.com>
2017-11-03 16:39:47 -0700

    files

--- profile/footer.php
+++ profile/footer.php
@@ -5,9 +5,11 @@
         Copyright © 2017 <span class="red">VET</span><span class="blue">RANS</span> LLC.
       </p>
 
-<!-- Quote -->
-
-<!-- success alert -->
+      <script
+        src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+        crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/semantic/dist/semantic.min.js"></script>
 
 </body>
 
