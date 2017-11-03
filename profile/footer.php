75afecb509f8b65b1f68e73f799d6b2386588127
Aaron <30958265+GoodZillo@users.noreply.github.com>
2017-11-03 03:33:21 -0700

    profile v2

---
+++ profile/footer.php
@@ -0,0 +1,14 @@
+      <p>
+        Call us to get your freight moved or your trucks covered. We Support Our Troops
+      </p>
+      <p>
+        Copyright © 2017 <span class="red">VET</span><span class="blue">RANS</span> LLC.
+      </p>
+
+<!-- Quote -->
+
+<!-- success alert -->
+
+</body>
+
+</html>
